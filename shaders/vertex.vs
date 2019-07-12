d8e11bc88f43474ae2347d0d7cb1d45f6a6d941f
Pieter PREEZ <ppreez@c4r7s4.wethinkcode.co.za>
2019-07-12 15:18:36 +0200

    Textures

--- shaders/vertex.vs
+++ shaders/vertex.vs
@@ -1,8 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
 out vec3 ourColor;
+out vec2 texCoord;
 
 uniform float xOffset;
 uniform float yOffset;
@@ -12,4 +14,5 @@ void main()
 {
     gl_Position = vec4(aPos.x + xOffset, aPos.y + yOffset, aPos.z +zOffset, 1.0);
     ourColor = aColor;
+    texCoord = aTexCoord;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 /*   By: ppreez <ppreez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 11:24:19 by ppreez            #+#    #+#             */
-/*   Updated: 2019/07/12 11:17:45 by ppreez           ###   ########.fr       */
+/*   Updated: 2019/07/12 15:18:08 by ppreez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define STB_IMAGE_IMPLEMENTATION
+// #define STB_IMAGE_IMPLEMENTATION
 #include "../includes/stb_image.h"
 
 #include "../glad/include/KHR/khrplatform.h"
@@ -66,19 +66,42 @@ int main()
 
     float vertices[] = 
     {
-         0.5f,   0.5f, 0.0f,    1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
-         0.5f,  -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-        -0.5f,  -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-        -0.5f,   0.0f, 0.0f,    1.0f, 1.0f, 0.0f,   0.0f, 1.0f
+         0.5f,   0.5f, 0.0f,    1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+         0.5f,  -0.5f, 0.0f,    0.5f, 0.5f, 0.5f,   1.0f, 0.0f,
+        -0.5f,  -0.5f, 0.0f,    0.0f, 0.0f, 0.0f,   0.0f, 0.0f,
+        -0.5f,   0.5f, 0.0f,    0.5f, 0.5f, 0.5f,   0.0f, 1.0f
     };
 
-    float tx_coord[] = 
+    unsigned int indices[] =
     {
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        0.5f, 1.0f
+        0, 1, 3,
+        1, 2, 3
     };
     
+    Shader ourShader("./shaders/vertex.vs", "./shaders/fragment.fs");
+
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
+    
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
     unsigned int texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -87,7 +110,6 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
     
     unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
     if (data)
@@ -96,48 +118,30 @@ int main()
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
-    {
         std::cout << "Texture loading failed." << std::endl;
-    }
-    
-    
     stbi_image_free(data);
 
-
-
-    Shader ourShader("./shaders/vertex.vs", "./shaders/fragment.fs");
-
-    unsigned int VAO;
-    unsigned int VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
     // float xOffset = 0.2f;
-    float yOffset = 0.3f;
+    // float yOffset = 0.3f;
     // float zOffset = 0.0f;
 
     while (!glfwWindowShouldClose(window))
     {
         
-        if (yOffset > -0.5)
-            ourShader.setFloat("yOffset", yOffset -= 0.1f);        
+        // if (yOffset > -0.5)
+            // ourShader.setFloat("yOffset", yOffset -= 0.1f);        
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         ourShader.use();
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     return (1);
 }
